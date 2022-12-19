25ccc01b0c6e786945b8c6908d77bbb59c672444
Silje Tanemsmo <siljetanemsmo@gmail.com>
2022-12-19 13:24:22 +0100

    Added documentation and reference to psPAS

--- Get-ConnectionComponentUsage.ps1
+++ Get-ConnectionComponentUsage.ps1
@@ -3,58 +3,59 @@
   Check if a Connection Component is in use by one or more platforms. 
 
 .DESCRIPTION
-  Get a list of platforms that are using a specific Connection Component (CC). 
-  Takes a CC ID (e.g NHN-SPLUNK-EXT) as input, 
-  searches through all active platforms listing those that have this CC configured.
+  Get a list of platforms using a specific Connection Component (CC). 
+  Takes a CC ID (e.g PSM-RDP), searches through all active platforms listing those that have this CC configured.
+  
+  Uses the pspas powershell module: https://github.com/pspete/psPAS
+  A valid pvwa session is required before running this script (see the pspas command New-PASSession)
+  
+.INPUTS
+  ConnCompID : The ID for the connection component you want to check
 
 .OUTPUTS
   Zero or more platforms using the specified CC.
+  
+      Platforms using the Connection Component PuTTY:
+
+      PlatformID  CC    CCEnabled
+      ----------  --    ---------
+      UnixSSH     PuTTY     False
+      UnixSSHKeys PuTTY     False
+  
+.EXAMPLE
+  .\Get-ConnectionComponentsByPlatforms.ps1 -ConnCompID PSM-RDP
 #>
 
-# ID PÅ CONNECTION COMPONENT MAN ØNSKER Å SJEKKE 
-$connCompName = "PuTTY" 
+param( 
+  [Parameter(Mandatory)]
+  [string]$ID
+)
 
-# REQUIRES A VALID SESSION TO PVWA 
-#New-NHNPASSession -credential (get-credential sa_pam_api)
+# REQUIRES A VALID SESSION TO PVWA (see https://github.com/pspete/psPAS for doc)
+# New-PASSession -Credential $cred -BaseURI https://pvwa.somedomain.com -type LDAP
 
-# Henter ut alle aktive plattformer
+# Gets all active platforms 
 $platforms = Get-PASPlatform -Active $true
 
 $platformsUsingCC = @()
 
-# Søker gjennom hver plattform
+# Searched through each platform
 foreach ($i in $platforms) {
-    #Write-Host "------------------------------------"
-    #write-host "`Searching throuh platform $($i.PlatformID)"
     $connComponents = (Get-PASPlatformPSMConfig -ID $i.Details.ID).PSMConnectors
 
     If ($connComponents.Count -le 0) {
         continue
     }
 
-    # Sjekker om spesifisert CC finnes i listen over CC'er på plattformen
-    If (($connComponents.PSMConnectorID).Contains($connCompName)) {
+    # Checks if specified CC exist in the list of all CC's in the platform
+    If (($connComponents.PSMConnectorID).Contains($ConnCompID)) {
         $Row = "" | Select-Object PlatformID,CC,CCEnabled
         $Row.PlatformID = $i.PlatformID
-        $Row.CC = $connCompName
-        $Row.CCEnabled = ($connComponents | Where-Object {$_.PSMConnectorID -eq "$connCompName"}).Enabled
+        $Row.CC = $ConnCompID
+        $Row.CCEnabled = ($connComponents | Where-Object {$_.PSMConnectorID -eq "$ConnCompID"}).Enabled
         $platformsUsingCC += $Row
-    } else {
-        #Write-Host "Platform $($i.PlatformID) har IKKE $connCompName." -ForegroundColor yellow
-    }   
+    }
 }
 
-Write-Host "`nPlatforms that are using the Connection Component $($connCompName):"
+Write-Host "`nPlatforms using the Connection Component $($ConnCompID):"
 $platformsUsingCC
-
-
-# OUTPUT:
-#
-# PS D:\Gitlab_ID\snippets> . 'd:\Gitlab_ID\snippets\Cyberark_pspas\Get-ConnectionComponentsByPlatforms.ps1'
-# 
-# Platforms that are using the Connection Component PuTTY:
-#
-# PlatformID  CC    CCEnabled
-# ----------  --    ---------
-# UnixSSH     PuTTY     False
-# UnixSSHKeys PuTTY     False
